28c9a977f5e29ff251fd89e7918d77ce85f9748e
delazzer27 <tomas.delazer@gmail.com>
2022-08-23 12:17:58 -0300

    agregar icono cart y listItemContainer

--- src/componentes/App.jsx
+++ src/componentes/App.jsx
@@ -1,41 +1,14 @@
 
 import './App.css';
-
+import CartWidgets from './CartWidgets/CartWidgets';
+import ItemListContainer from './ItemListContainer/ItemListContainer';
+import NavBar from './NavBar/NavBar';
 function App() {
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon" />
-    </button>
-    <div className="collapse navbar-collapse" id="navbarColor01">
-      <ul className="navbar-nav me-auto">
-        <li className="nav-item">
-          <a className="nav-link active" href="#">Home
-            <span className="visually-hidden">(current)</span>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Features</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Pricing</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">About</a>
-        </li>
-        
-      </ul>
-      <form className="d-flex">
-        <input className="form-control me-sm-2" type="text" placeholder="Search" />
-        <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-
+        <h1>Proyecto de React</h1>
+        <NavBar/>
+        <ItemListContainer saludo='Bienvenido a mi e-commerce'/>
     </div>
   );
 }
---
+++ src/componentes/CartWidgets/CartWidgets.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BiCart } from 'react-icons/bi'
+
+const CartWidgets = () => {
+    return (
+        <div>
+            <BiCart className='cart'/>
+        </div>
+    );
+}
+
+export default CartWidgets;
